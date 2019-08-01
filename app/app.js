9db18eee84e86c839053e257e774cf76bca6f784
Alex <dev.alex.ya@gmail.com>
2019-08-01 22:37:59 +0200

    add express to project

---
+++ app/app.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+    console.log('Example listening on port 3000');
+});
